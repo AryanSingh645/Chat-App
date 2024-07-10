1f5da92aa804a88f67b6b46dca7c617e63494d4c
AryanSingh645 <singhjee30476@gmail.com>
2024-07-10 11:51:30 +0530

    Added message routes

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { sendMessage } from "../controllers/message.controller.js";
+import { sendMessage, getMessage } from "../controllers/message.controller.js";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post('/send/:id', sendMessage);
+router.post('/send/:id', verifyJwt, sendMessage);
+router.get('/:id', verifyJwt, getMessage);
 
 export default router;
