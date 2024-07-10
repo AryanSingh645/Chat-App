782deec0ccec2ba52f6b1d2041f5be863f53e6c8
AryanSingh645 <singhjee30476@gmail.com>
2024-07-10 11:50:37 +0530

    Added message controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,7 +1,73 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Conversation } from "../models/conversation.model.js";
+import { Message } from "../models/message.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { ApiError } from "../utils/ApiError.js";
 
 const sendMessage = asyncHandler( async (req, res) => {
-    console.log("message", req.params.id);
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+        participants: {
+            $all : [senderId, receiverId]
+        }
+    })
+
+    if(!conversation){
+        conversation = new Conversation({
+            participants: [senderId, receiverId]
+        })
+    }
+
+    const newMessage = new Message({
+        senderId,
+        receiverId,
+        message
+    })
+
+    if(newMessage){
+        conversation.messages.push(newMessage._id)
+    }
+    
+    await Promise.all([conversation.save(), newMessage.save()]);
+
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        newMessage,
+        "Message sent successfully"
+    ))
 })
 
-export {sendMessage}
+const getMessage = asyncHandler( async (req, res) => {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+        participants: {
+            $all: [senderId, userToChatId]
+        }
+    }).populate("messages")
+    
+    if(!conversation){
+        throw new ApiError(400, [], "No conversation");
+    }
+
+    console.log(conversation);
+
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        conversation.messages,
+        "Message Fetched Successfully"
+    ))
+})
+
+export {
+    sendMessage,
+    getMessage
+}
